235b73383ef35ed214208f9742ffc3f985977630
Hadi Mahmoudi <hadim69@gmail.com>
2024-09-19 17:44:37 +0330

    rename to group overview

--- lib/src/features/group_performance/model/group_performance_data.dart
+++
@@ -1,8 +0,0 @@
-class GroupPerformanceData {
-  final String groupName;
-  final int activeLoanCount;
-  final double averageLoanSize;
-
-  GroupPerformanceData(
-      this.groupName, this.activeLoanCount, this.averageLoanSize);
-}
