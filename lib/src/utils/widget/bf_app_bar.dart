fe636aaca541749d0ae573a6efe1e36072590970
Hadi719 <hadi.m.69@gmail.com>
2024-10-17 21:00:43 +0330

    better AppBar

--- lib/src/utils/widget/bf_app_bar.dart
+++ lib/src/utils/widget/bf_app_bar.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 
 const double _kAppBarElevation = 16.0;
 
-AppBar bfAppBar({required String appBarTitle}) => AppBar(
+AppBar bfAppBar({
+  required String appBarTitle,
+  Widget? leading,
+  List<Widget>? actions,
+}) =>
+    AppBar(
       title: Text(appBarTitle),
       elevation: _kAppBarElevation,
+      leading: leading,
+      actions: [
+        if (actions != null) ...actions,
+      ],
     );
